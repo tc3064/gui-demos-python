33f50b0274183bd5d8590bdf72cd4ef15bab5b59
shenshan <shenshanpku@gmail.com>
2020-09-27 19:17:12 -0700

    remove the credentials from r

--- costagui/dj_tables.py
+++ costagui/dj_tables.py
@@ -1,8 +1,7 @@
 import datajoint as dj
-dj.config['database.host'] = 's1n4.u19motor.zi.columbia.edu'
-dj.config['database.user'] = 'RuiLab_test'
-dj.config['database.password'] = 'test1'
 
 lab = dj.create_virtual_module('lab', 'costa_lab')
 subject = dj.create_virtual_module('subject', 'costa_subject')
+action = dj.create_virtual_module('action', 'costa_action')
+acquisition = dj.create_virtual_module('acquisition', 'costa_acquisition')
 hardware = dj.create_virtual_module('hardware', 'costa_hardware')
