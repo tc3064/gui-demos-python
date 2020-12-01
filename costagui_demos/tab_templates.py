299b3875c09e45097c02b634698ac61d568f3fd1
shenshan <shenshanpku@gmail.com>
2020-12-01 08:40:01 -0800

    add all callback functions

--- costagui_demos/tab_templates.py
+++ costagui_demos/tab_templates.py
@@ -30,9 +30,10 @@ class TableBlock:
         self.table_name = table.__name__.lower()
         self.primary_key = table.heading.primary_key
         self.attrs = table.heading.attributes
+        self.field_names = table.heading.names
 
         self.display_table = component_utils.create_display_table(
-            table, f'{self.table_name}-table',
+            self.table, f'{self.table_name}-table',
             height=table_height, width=table_width)
 
         self.add_button = html.Button(
@@ -53,12 +54,12 @@ class TableBlock:
         )
 
         self.add_modal = component_utils.create_modal(
-            table, self.table_name, include_parts=include_parts,
+            self.table, self.table_name, include_parts=include_parts,
             mode='add'
         )
 
         self.update_modal = component_utils.create_modal(
-            table, self.table_name, include_parts=include_parts,
+            self.table, self.table_name, include_parts=include_parts,
             mode='update'
         )
 
@@ -104,7 +105,7 @@ class TableBlock:
 
                             html.Div(
                                 [
-                                    html.H6('Subject'),
+                                    html.H6(f'{self.table.__name__}'),
                                     self.display_table
                                 ],
                                 style={'marginRight': '1em',
@@ -123,9 +124,24 @@ class TableBlock:
     def callbacks(self, app):
 
         @app.callback(
+            [
+                Output(f'delete-{self.table_name}-button', 'disabled'),
+                Output(f'update-{self.table_name}-button', 'disabled')
+            ],
+            [
+                Input(f'{self.table_name}-table', 'selected_rows')
+            ])
+        def set_button_enabled_state(selected_rows):
+            if selected_rows:
+                disabled = False
+            else:
+                disabled = True
+            return disabled, disabled
+
+        @app.callback(
             Output(f'delete-{self.table_name}-confirm', 'displayed'),
             [Input(f'delete-{self.table_name}-button', 'n_clicks')])
-        def display_confirm(n_clicks):
+        def display_delete_confirm(n_clicks):
             if n_clicks:
                 return True
             return False
@@ -165,7 +181,135 @@ class TableBlock:
                 except Exception as e:
                     delete_message = delete_message + \
                         f'Error in deleting record {pk}: {str(e)}.'
-
-            data = self.table.fetch(as_dict=True)
+                data = self.table.fetch(as_dict=True)
+            elif triggered_component in (f'add-{self.table_name}-close',
+                                         f'update-{self.table_name}-close'):
+                data = self.table.fetch(as_dict=True)
 
             return data, delete_message
+
+        def toggle_modal(
+                n_open, n_close,
+                is_open, data, selected_rows,
+                modal_data, mode):
+
+            ctx = dash.callback_context
+            triggered_component = ctx.triggered[0]['prop_id'].split('.')[0]
+
+            if triggered_component == f'{mode}-{self.table_name}-button':
+                if selected_rows:
+                    modal_data = [data[selected_rows[0]]]
+                else:
+                    if mode == 'add':
+                        modal_data = [{k: '' for k in self.field_names}]
+                    elif mode == 'update':
+                        raise ValueError('Update Modal open without a particular row selected')
+
+                modal_open = not is_open if n_open or n_close else is_open
+
+            elif triggered_component == f'{mode}-{self.table_name}-close':
+                modal_open = not is_open if n_open or n_close else is_open
+
+            else:
+                modal_open = is_open
+
+            return modal_open, modal_data
+
+        @app.callback(
+            [
+                Output(f'add-{self.table_name}-modal', 'is_open'),
+                Output(f'add-{self.table_name}-table', 'data'),
+            ],
+            [
+                Input(f'add-{self.table_name}-button', 'n_clicks'),
+                Input(f'add-{self.table_name}-close', 'n_clicks'),
+            ],
+            [
+                State(f'add-{self.table_name}-modal', 'is_open'),
+                State(f'{self.table_name}-table', 'data'),
+                State(f'{self.table_name}-table', 'selected_rows'),
+                State(f'add-{self.table_name}-table', 'data'),
+            ],
+        )
+        def toggle_add_modal(*args):
+            return toggle_modal(*args, mode='add')
+
+        @app.callback(
+            [
+                Output(f'update-{self.table_name}-modal', 'is_open'),
+                Output(f'update-{self.table_name}-table', 'data'),
+            ],
+            [
+                Input(f'update-{self.table_name}-button', 'n_clicks'),
+                Input(f'update-{self.table_name}-close', 'n_clicks'),
+            ],
+            [
+                State(f'update-{self.table_name}-modal', 'is_open'),
+                State(f'{self.table_name}-table', 'data'),
+                State(f'{self.table_name}-table', 'selected_rows'),
+                State(f'update-{self.table_name}-table', 'data'),
+            ],
+        )
+        def toggle_update_modal(*args):
+            return toggle_modal(*args, mode='update')
+
+        @app.callback(
+            Output(f'add-{self.table_name}-message', 'value'),
+            [
+                Input(f'add-{self.table_name}-confirm', 'n_clicks'),
+                Input(f'add-{self.table_name}-close', 'n_clicks')
+            ],
+            [
+                State(f'add-{self.table_name}-table', 'data'),
+                State(f'add-{self.table_name}-message', 'value')
+            ]
+        )
+        def add_record(
+                n_clicks_add, n_clicks_close,
+                new_data, add_message):
+
+            ctx = dash.callback_context
+            triggered_component = ctx.triggered[0]['prop_id'].split('.')[0]
+
+            if triggered_component == f'add-{self.table_name}-confirm':
+
+                entry = {k: v for k, v in new_data[0].items() if v != ''}
+
+                add_message = 'Add message:'
+                try:
+                    pk = {k: v
+                          for k, v in callback_utils.clean_single_gui_record(
+                            entry, self.attrs).items()
+                          if k in self.primary_key}
+                    if (self.table & pk):
+                        add_message = add_message + \
+                            f'\nWarning: record {pk} exists in database'
+                    else:
+                        self.table.insert1(entry)
+                        add_message = add_message + \
+                            f'\nSuccessful insertion to {self.table_name}.'
+
+                except Exception as e:
+                    add_message = add_message + \
+                        f'\nError inserting into {self.table_name}: {str(e)}'
+
+            elif triggered_component == f'add-{self.table_name}-close':
+                add_message = 'Add message:'
+
+            return add_message
+
+        @app.callback(
+            Output(f'update-{self.table_name}-message', 'value'),
+            [
+                Input(f'update-{self.table_name}-confirm', 'n_clicks')
+            ],
+            [
+                State(f'update-{self.table_name}-table', 'data'),
+            ],
+        )
+        def update_record(
+                n_clicks, update_data):
+
+            new = update_data[0]
+            pk = {k: v for k, v in new.items() if k in self.primary_key}
+            return callback_utils.update_table(self.table, new, pk)
