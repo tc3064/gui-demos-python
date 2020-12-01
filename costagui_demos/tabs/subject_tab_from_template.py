3bc33c662de8288c8d39ea35309e13a92d8d0a48
shenshan <shenshanpku@gmail.com>
2020-11-30 17:24:35 -0800

    Create subject tab using TableTab class

---
+++ costagui_demos/tabs/subject_tab_from_template.py
@@ -0,0 +1,22 @@
+import datajoint as dj
+import dash
+import dash_table
+from dash.dependencies import Input, Output, State
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from costagui_demos.app import app
+from costagui_demos.dj_tables import lab, subject, hardware
+from costagui_demos import dj_utils, component_utils, callback_utils
+from costagui_demos.tab_templates import TableBlock
+import datetime
+
+
+subject_table_tab = TableBlock(subject.Subject, app)
+
+subject_table_tab.create_default_layout()
+
+if __name__ == '__main__':
+    dj.config['safemode'] = False
+    app.layout = subject_table_tab.layout
+    app.run_server(debug=True)
